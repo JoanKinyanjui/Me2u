7fab03fb71620ea4cb12671fddfc842dd71d1523
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2021-12-10 14:03:29 +0300

    another commit

--- template.js
+++ template.js
@@ -2,4 +2,7 @@ const timeline = gsap.timeline({defaults:{duration : 1}})
 
 timeline
 .from('.title',{opacity: 0, x: '-100vw', ease : 'elastic',rotation : 360,fontSize: '15px', stagger : .25})
-.from('.yellow', {rotation :720,duration: 10 ,color : 'blue' },'<1')
+.from('.yellow', {rotation :720,duration: 8 ,color : 'blue', repeat: -1, yoyo: true },'<1')
+
+
+
